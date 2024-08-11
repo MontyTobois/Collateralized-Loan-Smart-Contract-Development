775a2490120c3db04f6af0a5852bb1c083debb06
Andre Tobois <tobois.andre@gmail.com>
2024-08-11 01:49:10 -0400

    feat: Finished the needed functions and events for smart contract

--- contracts/Collateralized Loan.sol
+++ contracts/Collateralized Loan.sol
@@ -26,6 +26,7 @@ contract CollateralizedLoan {
     event LoanRequested(uint loanId, address borrower, uint collateralAmount, uint loanAmount, uint interestRate, uint dueDate);
     event LoanFunded(uint loanId, address borrower, uint loanAmount);
     event LoanRepaid(uint loanId, address lender, uint loanAmount);
+    event CollateralClaim(uint loanId, address lender,  uint collateralAmount);
 
     // Custom Modifiers
     // Hint: Write a modifier to check if a loan exists
@@ -46,7 +47,7 @@ contract CollateralizedLoan {
         // Hint: Calculate the loan amount based on the collateralized amount
         uint loanAmount = msg.value * 2;
         uint dueDate = block.timestamp + _duration;
-        // Hint: Increment nextLoanId and create a new loan in the loans mapping
+        // Hint: Create a new loan in the loans mapping
         loans[nextLoanId] = Loan({
             borrower: msg.sender,
             lender: address(0),
@@ -59,6 +60,7 @@ contract CollateralizedLoan {
         });
         // Hint: Emit an event for loan request
         emit LoanRequested(nextLoanId, msg.sender, msg.value, loanAmount, _interestRate, dueDate);
+       // Increments nextLoanId 
         nextLoanId++;
     }
 
@@ -66,6 +68,7 @@ contract CollateralizedLoan {
     // Hint: Write the fundLoan function with necessary checks and logic
     function fundLoan (uint loanId) external payable loanExists(loanId) notFunded(loanId) {
         Loan storage loan = loans[loanId];
+        // checks if Loan amount is correct
         require(msg.value == loan.loanAmount, "Incorrect loan amount");
 
         loan.isFunded = true;
@@ -78,11 +81,14 @@ contract CollateralizedLoan {
     // Hint: Write the repayLoan function with necessary checks and logic
     function repayLoan(uint loanId) external payable loanExists(loanId){
          Loan storage loan = loans[loanId];
+         // checks if Loan is funded
          require(loan.isFunded, "Loan not funded");
+         // checks if Loan is repaid
          require(!loan.isRepaid, "Loan already repaid");
+         // checks if msg.sender is the Borrower of the Loan
          require(msg.sender == loan.borrower, "Only the borrower can repay loan");
 
-         // checks if loan is repaid
+         
         uint repayAmount = loan.loanAmount + (loan.loanAmount * loan.interestRate/100);
         require(msg.value == repayAmount, "Incorrect repayment amount");
 
@@ -94,4 +100,22 @@ contract CollateralizedLoan {
 
     // Function to claim collateral on default
     // Hint: Write the claimCollateral function with necessary checks and logic
+    function claimCollateral(uint loanId) external loanExists(loanId){
+         Loan storage loan = loans[loanId];
+         // Checks if Loan is funded
+         require(loan.isFunded, "Loan not funded");
+         // Checks if Loan is repaid
+         require(!loan.isRepaid, "Loan already repaid");
+         // Checks the timeframe of the loan
+         require(block.timestamp > loan.dueDate, "Loan is not due yet");
+         // Checks to see if lender is claiming the collateral
+         require(msg.sender == loan.lender, "Only the lender can claim Collateral");
+
+
+         uint collateralAmount = loan.collateralAmount;
+         loan.collateralAmount = 0; // avoid reentrancy attacks
+         payable(loan.lender).transfer(collateralAmount);
+
+         emit CollateralClaim(loanId, msg.sender, collateralAmount);
+    }
 }
