79f791247917e93fb4c1a0f4988692e71819a8e6
Andre Tobois <tobois.andre@gmail.com>
2024-08-10 23:55:07 -0400

    Added necessary variables, modifiers and worked on the request loan function

--- contracts/Collateralized Loan.sol
+++ contracts/Collateralized Loan.sol
@@ -7,8 +7,15 @@ contract CollateralizedLoan {
     struct Loan {
         address borrower;
         // Hint: Add a field for the lender's address
+        address lender;
         uint collateralAmount;
         // Hint: Add fields for loan amount, interest rate, due date, isFunded, isRepaid
+        uint loanAmount;
+        uint interestRate; //Interest rate in percentages 
+        uint dueDate; // Timestamp when the loan is due
+        bool isFunded;
+        bool isRepaid;
+        
     }
 
     // Create a mapping to manage the loans
@@ -16,23 +23,46 @@ contract CollateralizedLoan {
     uint public nextLoanId;
 
     // Hint: Define events for loan requested, funded, repaid, and collateral claimed
+    event LoanRequested(uint loanId, address borrower, uint collateralAmount, uint loanAmount, uint interestRate, uint dueDate);
 
     // Custom Modifiers
     // Hint: Write a modifier to check if a loan exists
+    modifier loanExists(uint loanId) {
+        require(loanId < nextLoanId, "Loan does not exist.");
+        _;
+    }
     // Hint: Write a modifier to ensure a loan is not already funded
+    modifier notFunded(uint loanId) {
+        require(!loans[loanId].isFunded, "Loan is funded already.");
+        _;
+    }
 
     // Function to deposit collateral and request a loan
     function depositCollateralAndRequestLoan(uint _interestRate, uint _duration) external payable {
         // Hint: Check if the collateral is more than 0
+        require(msg.value > 0, "collateral amount must be greater the 0");
         // Hint: Calculate the loan amount based on the collateralized amount
-
+        uint loanAmount = msg.value * 2;
+        uint dueDate = block.timestamp + _duration;
         // Hint: Increment nextLoanId and create a new loan in the loans mapping
+        loans[nextLoanId] = Loan({
+            borrower: msg.sender,
+            lender: address(0),
+            collateralAmount: msg.value,
+            loanAmount: loanAmount,
+            interestRate: _interestRate,
+            dueDate: dueDate,
+            isFunded: false,
+            isRepaid: false
+        });
         // Hint: Emit an event for loan request
+        emit LoanRequested(nextLoanId, msg.sender, msg.value, loanAmount, _interestRate, dueDate);
+        nextLoanId++;
     }
 
     // Function to fund a loan
     // Hint: Write the fundLoan function with necessary checks and logic
-
+    
     // Function to repay a loan
     // Hint: Write the repayLoan function with necessary checks and logic
 
